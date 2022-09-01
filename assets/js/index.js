b55ea459678562255110800db7ef82c5799e6cfa
first-name second-name <silvia.wachira@student.moringa.com>
2022-09-01 22:17:05 +0300

    get an api key

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,9 @@
 
-
+"apikey": "087d2d5670b960565c41ce98b2fb0cfa"
 const form = document.querySelector("form")
 form.addEventListener("submit", (e)=> {
     e.preventDefault;
 //     alert("Hello")
     // e.text.value;
-
 
 })
