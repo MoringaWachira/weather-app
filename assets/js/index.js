f9182161d2005999da4ec955ee870d45256f7b6c
first-name second-name <silvia.wachira@student.moringa.com>
2022-09-03 08:06:29 +0300

    add a response.json

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,17 @@
 "apikey": "087d2d5670b960565c41ce98b2fb0cfa"
 const txt = document.querySelector('#text');
 const btn = document.querySelector('#btn');
-// form.addEventListener("submit", (e)=> {
-//     e.preventDefault;
-//     alert("Hello")
-    // e.text.value;
+const showWeatherData document.querySelector('.showWeatherData');
 
-// })
+// add an event listener
+btn.addEventListener("click", ()=> {
+    const valueOfCity = txt.value;
+    console.log(valueOfCity);
+
+    // fetch data from the API using get
+    fetch(``)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+})
