a1aba8d0703b977d368353948b6cbb7ea52ab6ab
first-name second-name <silvia.wachira@student.moringa.com>
2022-09-05 15:38:16 +0300

    style the ID submit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,25 @@
 // get API Key from openweather
-"apikey": "087d2d5670b960565c41ce98b2fb0cfa"
-const txt = document.querySelector('#text');
-const btn = document.querySelector('#btn');
-const showWeatherData document.querySelector('.showWeatherData');
+// "apikey": "087d2d5670b960565c41ce98b2fb0cfa"
+// const txt = document.querySelector('#text');
+// const btn = document.querySelector('#btn');
+const showWeatherData = document.querySelector('.showWeatherData');
+
+const API_Key = "087d2d5670b960565c41ce98b2fb0cfa";
 
 // add an event listener
-btn.addEventListener("click", ()=> {
-    const valueOfCity = txt.value;
-    console.log(valueOfCity);
 
-    // fetch data from the API using get
-    fetch(``)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    })
-})
+
+const form = document.querySelector("form")
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    
+    const valueOfCity = txt.value;
+    return(valueOfCity);
+
+    // fetch data from the API using GET
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${valueOfCity}&apikey=087d2d5670b960565c41ce98b2fb0cfa`)
+        .then(res => res.json())
+        .then(data => {
+             console.log(data);
+    });
+});
